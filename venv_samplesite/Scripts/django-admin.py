dfff3ea9d1e2438a089e09a5a94a8bd27d894f91
Ivan Terehin <ITerex91@yandex.ru>
2020-04-04 13:05:15 +0300

    Редактирование модели, добавление admin, редактирование отображение Административной панели

--- venv_samplesite/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\Learning\Django_projects\samplesite\venv_samplesite\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ venv_samplesite/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ivan/Django_projects/samplesite/venv_samplesite/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
